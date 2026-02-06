74a0070754b73d951327c7eb3542d0ad534d97f0
Alice-M208 <alice.michajlova@seznam.cz>
2026-02-06 10:29:34 +0100

    6.2.2026

---
+++ 2.lekce/script.js
@@ -0,0 +1,12 @@
+alert("Zkouška")
+alert(3)
+
+let userName = "Miky";
+console.log(userName); 
+
+userName = "Maggie";
+console.log(userName);
+
+console.log(7+4)
+console.log("7"+"4");
+console.log("7"+4)
