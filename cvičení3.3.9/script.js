fa2ecdfa5bbc18ed3fff73ccd5bd246586651ff3
Alice-M208 <alice.michajlova@seznam.cz>
2026-05-29 10:35:51 +0200

    3.39

---
+++ cvičení3.3.9/script.js
@@ -0,0 +1,12 @@
+function deviding(a,b) {
+    let result = a % b;
+    return result
+}
+{
+      if (result += 2) {
+        return true;
+      } else {
+        return false;
+      }
+}
+console.log(deviding(2, 9))
